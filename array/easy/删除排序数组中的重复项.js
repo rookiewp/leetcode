3daecb98ddbc9964be6307621a40d064efd7858a
wangpei <2297620702@qq.com>
2020-06-02 11:03:34 +0800

    删除排序数组中的重复项修改

--- array/easy/删除排序数组中的重复项.js
+++ array/easy/删除排序数组中的重复项.js
@@ -10,18 +10,16 @@
  * @param {number[]} nums
  * @return {number}
  */
-var removeDuplicates = function(nums) {
-  let len = 1;
-  for (let i = 0; i < nums.length -1; i++) {
-    if (nums[i] !== nums[i+1]) {
-      nums[i] = nums[i+1];
-      len++
+var removeDuplicates = function (nums) {
+  if (nums.length <= 1) {
+    return nums.length;
+  }
+  let j = 0;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[j]) {
+      j++;
+      nums[j] = nums[i];
     }
   }
-  return len;
+  return nums;
 };
-// const arr = removeDuplicates([1,1,1,2,3,4,4,5,5,7,9])
-
-
-
-
