aee67f7afa517f7258e60d68e17c3617fdfa75c8
wangpei <2297620702@qq.com>
2020-08-13 10:59:34 +0800

    搜索插入位置

--- binary-search/easy/搜索插入位置.js
+++ binary-search/easy/搜索插入位置.js
@@ -24,11 +24,28 @@
  * @param {number} target
  * @return {number}
  */
-var searchInsert = function(nums, target) {
+function searchInsert(nums, target) {
   for (let i = 0; i < nums.length; i++) {
     const currentValue = nums[i];
     if (currentValue < target) continue;
     if (currentValue >= target) return i;
   }
   return nums.length;
-};
+}
+
+// 二分查找：查找“第一个大于等于”target的值，如果找不到，则插入数组最后
+function searchInsert2(nums, target) {
+  let head = 0;
+  let tail = nums.length - 1;
+  while (head <= tail) {
+    const mid = Math.floor((head + tail) / 2);
+    if (nums[mid] < target) {
+      head = mid + 1;
+    } else {
+      if (mid === 0 || nums[mid - 1] < target) return mid;
+      tail = mid - 1;
+    }
+  }
+  // 如果找不到，则插入数组最后
+  return nums.length;
+}
